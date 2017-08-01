8a9479f207997a408bda65c9ddbf6d616eaed3bc
admin-m <977986310@qq.com>
2017-08-01 18:38:38 +0800

    优化

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,36 @@
-'use strict';
+function count_same_elements(collection) {
+  let sameElementCount = [];
 
-function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  collection.map(element => {
+    let nameIndex = findContainsKeyIndex(sameElementCount, element);
+    if (nameIndex !== -1) addKeyCount(sameElementCount, nameIndex, element);
+    else sameElementCount.push(createObj(element));
+  });
+
+  return sameElementCount;
 }
+
+function findContainsKeyIndex(sameElementCount, name) {
+  return sameElementCount.findIndex(element => element.name === name.charAt(0));
+}
+
+function addKeyCount(sameElementCount, nameIndex, element) {
+  let [name, summary] = getKeyCount(element);
+  sameElementCount[nameIndex].summary += summary;
+}
+
+function createObj(element) {
+  let [name, summary] = getKeyCount(element);
+
+  return {name: name, summary: summary};
+}
+
+function getKeyCount(name) {
+  let nameCount = name.replace(/-|\[|\]|:/g, " ").split(" ");
+
+  if (nameCount.length === 1) return [name, 1];
+
+  return [name[0], parseInt(nameCount[1])];
+}
+
+module.exports = count_same_elements;
